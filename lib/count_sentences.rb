be64450add7d2c6b853dc6aa42c1b4021ad4e3dd
Jazmin Matos <jmatos1993@gmail.com>
2020-09-04 00:57:12 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,35 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+     if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
-  def count_sentences
-
+  def count_sentences #call on a string 
+    counting = []
+    counting << self.split(/\W/)
+    counting.count 
   end
+  
+  #use self keyword
+  
 end
